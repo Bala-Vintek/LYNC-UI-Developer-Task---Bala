c192c2d3b3d72fc1e5718af9448bc6ffc64fcd0c
Bala-Vintek <balasubramani.g@vinteksolution.com>
2024-09-28 19:59:12 +0530

    Updated

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import "../App.css";
+import "../Components/responsive.css";
 import NavIcon from "../images/Group 12.svg";
 import Arrow from "../images/Arrow_alt_lright_alt.svg";
+import ArrowWhite from "../images/Arrow_alt_lright.svg";
+
 const Home = () =>{
+    const toggleMenu = () => {  
+        const mobileMenu = document.querySelector('.mobile-menu');  
+        mobileMenu.classList.toggle('active');  
+    };  
     return(
         <div>
         <div className='sec1'>
             <div className='navbar'>
-                    <div><img src={NavIcon} alt="img" /></div>
+                <div className="hamburger" onClick={toggleMenu}>  
+                    <div className="bar"></div>  
+                    <div className="bar"></div>  
+                    <div className="bar"></div>  
+                </div>
+                <div><img src={NavIcon} alt="img" /></div>
                     <div className='nav-center'>
                     <div>Home</div>
                     <div>Documentation</div>
@@ -15,18 +27,27 @@ const Home = () =>{
                     <div>LYNC Portal</div>
                     <div>Request a demo</div>
                     </div>
-                    <button>Get Started <img src={Arrow} alt="img" /></button>
+                <button><div className='get-starts'>Get Started</div> <img src={Arrow} alt="img" /></button>  
             </div>
+            <div className="mobile-menu">  
+                <div className='nav-item'>Home</div>  
+                <div className='nav-item'>Documentation</div>  
+                <div className='nav-item'>Blogs</div>  
+                <div className='nav-item'>LYNC Portal</div>  
+                <div className='nav-item'>Request a demo</div>
+                <div className='nav-item' onClick={toggleMenu}>Close</div>  
+            </div>  
         </div>
         <div className='sec2'>
             <div className='home-cont'>Launch Your Game</div>
             <div className='home-cont'>on <span>Web 3.0</span> in Minutes</div>
-        <div className='home-cont-secon'><span>We help game developers to launch their game on Web 3.0, by keeping </span><span>
-            blockchain complexities aside with WebWorld SDKs!</span>
+        <div className='home-cont-secon'>
+            <span>We help game developers to launch their game on Web 3.0, by keeping </span>
+            <span>blockchain complexities aside with WebWorld SDKs!</span>
         </div>
         <div className='btn-wrap'>
-            <button className='get-start'>Get started <img src={Arrow} alt="img" /></button>
-            <div>View Docs<img src={Arrow} alt="img" /></div>
+            <button className='get-start'>Get started <img src={ArrowWhite} alt="img" /></button>
+            <div className='view-docs'>View Docs<img src={ArrowWhite} alt="img" /></div>
         </div>
         </div>
         
--- src/Components/Section2.js
+++ src/Components/Section2.js
@@ -1,10 +1,51 @@
 import React from 'react';
 import "../App.css";
+import SecImage from "../images/LongHashX White 1.svg";
+import SecImage2 from "../images/Axelar Logo Full White 1.svg";
+import SecImage3 from "../images/image 39.svg";
+import Line from "../images/Line 250.svg";
+import Sec5Image from "../images/Blockchain/image 88.svg";
+import Sec5Image2 from "../images/Blockchain/image 89.svg";
+import Sec5Image3 from "../images/Blockchain/SOM_Website_Logo (1) 1.svg";
+import Sec5Image4 from "../images/Blockchain/image 90.svg";
+import Sec5Image5 from "../images/Blockchain/image 87.svg";
 
 const Home = () =>{
     return(
         <div>
-        <div className='sec2'></div>
+        <div className='sec3'>
+            <div>Backed by</div>
+            <div className='sec3-container'>
+                <img src={SecImage} alt="img" />
+                <img src={SecImage2} alt="img" />
+                <img src={SecImage3} alt="img" />
+            </div>
+        </div>
+        <div className='sec-wrap'>
+        <div className='sec4'>
+            <img src={Line} alt="img" />
+            <div className='sec4-cont'>LYNC provides a scalable infrastructure for launching web3 games, without hampering the <span>gaming experience.</span>So that you can deliver the next big hit!</div>
+            <img src={Line} alt="img" className='sec4-bot-line'/>
+        </div>
+        <div className='sec5'>
+            <div className='sec5-cont'>Making blockchain gaming <br />accessible to <span>everyone!</span></div>
+            <div className='sec5-img-container'>
+            <img src={Sec5Image} alt="img" />
+            <img src={Sec5Image2} alt="img" />
+            <img src={Sec5Image3} alt="img" />
+            <img src={Sec5Image4} alt="img" />
+            <img src={Sec5Image5} alt="img" />
+            </div>
+        </div>
+        </div>
+        <div className='sec6'>
+            <div className='sec6-cont'>Power Up Your Web3 Game Development, With <span>LYNC SDKs</span></div>
+            <div className='sec6-sub-cont'>LYNC SDKs can be easily integrated into game engines like Unity 3D and Unreal Engine in a few clicks.</div>
+        </div>
+        <div className='footer'>
+        <div className='sec6-cont'>Power Up Your Web3 Game Development, With <span>LYNC SDKs</span></div>
+        <div className='sec6-sub-cont'>LYNC SDKs can be easily integrated into game engines like Unity 3D and Unreal Engine in a few clicks.</div>
+        </div>
         </div>
     )
 }
